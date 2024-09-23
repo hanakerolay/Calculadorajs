724988df3b23577e4cca0d0d0bee656b2903ccf1
Kerolay <hana.kerolay@gmail.com>
2024-09-23 17:19:39 -0300

    subircalculadora

--- js/script.js
+++ js/script.js
@@ -91,3 +91,27 @@ function segundograu() {
         document.getElementById("segundograu").innerText = "x1 = " + x1 + " -- " + "x2 = " + x2;
     }
 }
+
+
+
+
+function calcular() {
+    let catetoOposto = Number(document.getElementById('catetoOposto').value);
+    let catetoAdjacente =Number(document.getElementById('catetoAdjacente').value);
+
+    // Calcular a hipotenusa usando o Teorema de Pitágoras
+    let hipotenusa = Math.sqrt(Math.pow(catetoOposto, 2) + Math.pow(catetoAdjacente, 2));
+
+    // Calcular seno, cosseno e tangente
+    let seno = catetoOposto / hipotenusa;
+    let cosseno = catetoAdjacente / hipotenusa;
+    let tangente = catetoOposto / catetoAdjacente;
+
+    // resultados
+    document.getElementById('resultado').innerHTML = `
+        <p><strong>Hipotenusa:</strong> ${hipotenusa.toFixed(2)}</p>
+        <p><strong>Seno:</strong> ${seno.toFixed(2)}</p>
+        <p><strong>Cosseno:</strong> ${cosseno.toFixed(2)}</p>
+        <p><strong>Tangente:</strong> ${tangente.toFixed(2)}</p>
+    `;
+}
