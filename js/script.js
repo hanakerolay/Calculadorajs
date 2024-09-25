c12f3299636e0be2f6109bc1ae91d6da868e62ce
Kerolay <hana.kerolay@gmail.com>
2024-09-25 00:54:06 -0300

    subiralterac

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ function retagulo() {
     let base = Number(document.getElementById("baseret").value)
     let altura = Number(document.getElementById("alturaret").value)
     let areadoretangulo = base * altura
-    document.getElementById("areadoretangulo").innerText = areadoretangulo
+    document.getElementById("areadoretangulo").innerText = "O valor do retagulo é: " + areadoretangulo
 }
 
 function triangulo() {
     let basetria = Number(document.getElementById("basetria").value)
     let alturatria = Number(document.getElementById("alturatria").value)
     let areadotriangulo = (basetria * alturatria)/2
-    document.getElementById("areadotriangulo").innerText = areadotriangulo
+    document.getElementById("areadotriangulo").innerText = "O valor do triangulo é: " + areadotriangulo
 }
 
 function losango() {
     let diagonalmaior = Number(document.getElementById("diagonalmaior").value)
     let diagonalmenor = Number(document.getElementById("diagonalmenor").value)
     let areadodiagonal = (diagonalmaior * diagonalmenor)/2
-    document.getElementById("areadodiagonal").innerText = areadodiagonal
+    document.getElementById("areadodiagonal").innerText = "O valor do losango é: " + areadodiagonal
 }
 
 function trapezio() {
@@ -27,13 +27,13 @@ function trapezio() {
     console.log(basemenor)
     console.log(alturatrap)
     let areadotrapezio = ((basemaior + basemenor)*alturatrap)/2
-    document.getElementById("areadotrapezio").innerText = areadotrapezio
+    document.getElementById("areadotrapezio").innerText = "O valor do trapezio é: " + areadotrapezio
 }
 
 function cubo() {
     let base = Number(document.getElementById("basecubo").value)
     let areadocubo = base *3
-    document.getElementById("areadocubo").innerText = areadocubo
+    document.getElementById("areadocubo").innerText = "O valor do cubo é: " + areadocubo
 }
 
 function paralelepípedo() {
@@ -44,14 +44,14 @@ function paralelepípedo() {
     console.log(ladoB)
     console.log(ladoC)
     let areadoparalelepípedo = ladoA * ladoB * ladoC
-    document.getElementById("areadoparalelepípedo").innerText = areadoparalelepípedo
+    document.getElementById("areadoparalelepípedo").innerText = "O valor do paralelepípedo é: " + areadoparalelepípedo
 }
 
 function calcAreaesfera() {
     let raio = Number(document.getElementById("esfera").value)
     const pi = 3.14;
     let Areaesfera = 4 * pi * (raio ** 2)
-    document.getElementById("calcAreaesfera").innerText = "A área da esfera é: " + Areaesfera.toFixed(2)
+    document.getElementById("calcAreaesfera").innerText = "A área da esfera é: " + Areaesfera
 }
 
 function calcAreaCilindro() {
@@ -59,7 +59,7 @@ function calcAreaCilindro() {
     let alturaCilin = Number(document.getElementById("alturaCilin").value)
     const pi = 3.14;
     let calcVolumeCilindro = pi * raio ** 2 * alturaCilin
-    document.getElementById("AreaCilin").innerText = "O volume do cilindro é: " + calcVolumeCilindro.toFixed(2)
+    document.getElementById("AreaCilin").innerText = "O volume do cilindro é: " + calcVolumeCilindro
 }
 
 function calcAreaCone() {
@@ -67,13 +67,13 @@ function calcAreaCone() {
     let alturacon = Number(document.getElementById("alturacon").value)
     const pi = 3.14;
     let calcAreaCone = pi * raiocon ** 2 * alturacon * 0.33
-    document.getElementById("AreaCone").innerText = "O volume do Cone é: " + calcAreaCone.toFixed(2)
+    document.getElementById("AreaCone").innerText = "O volume do Cone é: " + calcAreaCone
 }
 
 function piramide() {
     let ladoPiram = Number(document.getElementById("ladopiram").value);
     let alturaPira = Number(document.getElementById("alturapira").value);
     let volumePiramide = (ladoPiram ** 2 * alturaPira) / 3;
-    document.getElementById("areapiramide").innerText = "O volume da pirâmide é: " + volumePiramide.toFixed(2)
+    document.getElementById("areapiramide").innerText = "O volume da pirâmide é: " + volumePiramide
 }
 
